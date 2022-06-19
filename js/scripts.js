bb5f90f307ffd74bb1374188965d69888c896ef3
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2022-06-19 13:46:27 +0200

    add array+add array elements
    Signed-off-by: khaledawaza <105608383+khaledawaza@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-
-alert('Hello world');
-
-let favoriteFood= 'mango';
-document.write(favoriteFood);
-
+   let pokemonList = []
+   pokemonList[0] = {name: 'Butterfree', type: ['flying', 'poison'], height:7}
+    pokemonList[1] = {name: 'Charizard', type: ['fire', 'flying'], height:7}
+    pokemonList[2] = {name: 'Starfish', type: 'water', height:7}
