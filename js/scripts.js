3cf7ad4be8deaf1d29eb03f9371f1ce3583f2fc6
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2022-07-01 00:55:42 +0200

    Update scripts.js
    Signed-off-by: khaledawaza <105608383+khaledawaza@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 let pokemonList = [
-  {name: "charizard"   ,height: 1.7, types: ["fire","flying",]},
-  {name: "Bulbasur" ,height: 0.5, types: ["grass"   ,"poison"]},
-  {name: "Pikachu"    ,height: 0.4, types: ["ground"  ,"electric","poison"]},
-  {name: "butterFree" ,height: 0.7, types: ["bug" ,"ice","flying"]},
-  {name: "kadabra"    ,height: 1.1, types: ["psychic"  ,"fire", "poison"]}
+  {name: "charizard",
+   height: 1.7,
+   types: ["fire","flying",]},
+
+  {name: "Bulbasur",
+   height: 0.5, 
+   types: ["grass","poison"]},
+
+  {name: "Pikachu", 
+    height: 0.4,
+    types: ["ground","electric","poison"]},
+
+  {name: "butterFree",
+   height: 0.7, 
+   types: ["bug","ice","flying"]},
+
+  {name: "kadabra",
+   height: 1.1,
+   types: ["psychic","fire", "poison"]}
 ];
 
 for (let i=0; i<pokemonList.length; i++){
