20bd340527b5c09f8944492f5cd32a0f5cdc4bf8
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2022-07-01 00:49:44 +0200

    Update scripts.js
    Use document.write() inside the loop’s code
    
    Signed-off-by: khaledawaza <105608383+khaledawaza@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ let pokemonList = [
   {name: "kadabra"    ,height: 1.1, types: ["psychic"  ,"fire", "poison"]}
 ];
 
+for (let i=0; i<pokemonList.length; i++){
+  document.write(pokemonList[i].name +" "+"height is "+ pokemonList[i].height +" m!"); 
+
+  if(pokemonList[i].height >= 1.7) {
+      document.write("     - I\`m size L. Wow, that\`s big!"+ "<br>");
+  } else if(pokemonList[i].height >=0.5 && pokemonList[i].height <=1.1) {
+      document.write("     - I\`m size M. "+ "<br>");
+  } else {
+      document.write("     - I\`m size S. " + "<br>");
+  }
+}
+
