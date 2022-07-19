f40d6b2f174cc0b0c2019fe1f09b680cf4215f44
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2022-07-19 13:45:44 +0200

    addListItem in return and create a button Add the addListItem() function to the object you’re returning from the IIFE
    Signed-off-by: khaledawaza <105608383+khaledawaza@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,26 +22,40 @@ let pokemonRepository = (function(){
          types: ["psychic","fire", "poison"]}
       ];
 
-    function add(pokemon){
+      function add(pokemon){
         pokemonList.add(pokemon);
     }
     function getAll(){
         return pokemonList;
     }
+    function addListItem(pokemon){
+     let pokemonList = document.querySelector(".pokemon-list");
+     let listItem = document.createElement("li");
+     let button = document.createElement("button");
+     button.innerText = pokemon.name;
+     button.classList.add("pokemon-button");
+     listItem.appendChild(button);
+     pokemonList.appendChild(listItem);
+     eventListener(button, pokemon);    
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
+    function eventListener (button, pokemon){
+        button.addEventListener("click", function(){
+            showDetails(pokemon)
+        });
+    }
     return {
         add,
-        getAll
+        getAll,
+        addListItem
     }
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon){
-    createElement(pokemon.name +" "+"height is "+ pokemon.height +" m!"); 
 
-    if(pokemon.height >= 1.7) {
-        document.write("     - I\`m size L. Wow, that\`s big!"+ "<br>");
-    } else if(pokemon.height >=0.5 && pokemon.height <=1.1) {
-        createElement("     - I\`m size M. "+ "<br>");
-    } else {
-        createElement("     - I\`m size S. " + "<br>");
-    }
-});
+
+    pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+    });
+
