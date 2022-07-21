eac93da96ecfe3485e2ce680c6fad2858c5fa80c
khaledawaza <105608383+khaledawaza@users.noreply.github.com>
2022-07-21 17:37:02 +0200

    delete my  pokemon frome (pokemon List)
    Signed-off-by: khaledawaza <105608383+khaledawaza@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,5 @@
   let pokemonRepository = (function () {
-        let pokemonList = [
-            
-            {name: "charizard",
-             height: 1.7,
-             types: ["fire","flying",]},
-          
-            {name: "Bulbasur",
-             height: 0.5, 
-             types: ["grass","poison"]},
-          
-            {name: "Pikachu", 
-              height: 0.4,
-              types: ["ground","electric","poison"]},
-          
-            {name: "butterFree",
-             height: 0.7, 
-             types: ["bug","ice","flying"]},
-          
-            {name: "kadabra",
-             height: 1.1,
-             types: ["psychic","fire", "poison"]}
-            ];
+        let pokemonList = [];
         let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
       
         function add(pokemon) {
